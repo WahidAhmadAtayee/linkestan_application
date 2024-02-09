075cbcc7d8d27ce622e3b89d18a8c4de2998b565
Mr.Atayee <wahidahmadatayee382@gmail.com>
2024-02-09 10:01:40 +0430

    Designed the splash_screen

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -6,7 +6,78 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("Linkestan Application"),
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              color: Color.fromRGBO(255, 0, 0, 1),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(260),
+                  ),
+                  color: Color.fromRGBO(255, 255, 255, 1),
+                ),
+                width: double.infinity,
+                height: 500,
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                    top: 30.0,
+                    left: 90.0,
+                    right: 50.0,
+                    bottom: 20.0,
+                  ),
+                  child: Image.asset(
+                    "images/LinkestanLogo.png",
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Color.fromRGBO(255, 255, 255, 1),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(167),
+                  ),
+                  color: Color.fromARGB(255, 255, 0, 0),
+                ),
+                width: double.infinity,
+                height: 180,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Welcome to\nLinkestan App",
+                      style: TextStyle(
+                        color: Color.fromRGBO(255, 255, 255, 1),
+                        fontSize: 50,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 85.0,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Version 0.1",
+                          style: TextStyle(
+                            color: Color.fromRGBO(255, 255, 255, 1),
+                            fontSize: 15,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
